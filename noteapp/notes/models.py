33f2ef0feb2fb8451f5485527ec9a4123be565af
Noah Ho <hoduy.ann@gmail.com>
2026-01-29 17:07:13 +0700

    Update form, update models

--- noteapp/notes/models.py
+++ noteapp/notes/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class Note(models.Model):
     content = models.TextField()
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- noteapp/notes/views.py
+++ noteapp/notes/views.py
@@ -1,18 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django import forms
 
 from notes.models import Note
 
 # Create your views here.
+class NoteForm(forms.Form):
+    note = forms.CharField(max_length=100)
+    status = forms.BooleanField(required=False)
+
 def index(request):
     if request.method == "POST":
-        content = request.POST.get("content")
-        if content:
-            Note.objects.create(content=content)
+        Form = NoteForm(request.POST)
+        if Form.is_valid():
+            note = Form.cleaned_data["note"]
+            status = Form.cleaned_data["status"]
+            Note.objects.create(content=note,status=status)
         return redirect("index")
-    
+    else:
+        Form = NoteForm()
+
     notes = Note.objects.all()
     return render(request, "notes/index.html", {
-        "notes": notes
+        "notes": notes,
+        "form": Form,
     })
 
