fc9a513203ec4d2f381890bad232bd1763fe5206
Duyan21 <128144830+Duyan21@users.noreply.github.com>
2026-01-29 21:17:32 +0700

    Add login and logout funtion (#2)

--- noteapp/notes/views.py
+++ noteapp/notes/views.py
@@ -1,13 +1,21 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from django import forms
 
 from notes.models import Note
 
 # Create your views here.
+
+
 class NoteForm(forms.Form):
     note = forms.CharField(max_length=100)
     status = forms.BooleanField(required=False)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Username"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
+
 
 def index(request):
     if request.method == "POST":
@@ -15,7 +23,7 @@ def index(request):
         if Form.is_valid():
             note = Form.cleaned_data["note"]
             status = Form.cleaned_data["status"]
-            Note.objects.create(content=note,status=status)
+            Note.objects.create(content=note, status=status)
         return redirect("index")
     else:
         Form = NoteForm()
@@ -26,3 +34,25 @@ def index(request):
         "form": Form,
     })
 
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect("index")
+            else:
+                form.add_error(None, "Invalid credentials")
+    else:
+        form = LoginForm()
+
+    return render(request, "notes/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
